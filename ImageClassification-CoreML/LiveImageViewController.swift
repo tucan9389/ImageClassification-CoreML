10a32e495c8b1d46a25b3579f6680aa719e78fd2
tucan9389 <tucan9389@gmail.com>
2019-07-20 10:53:24 +0900

    Change project name and add new classification model(MobileNetV2, Resnet50, SqueezeNet) downloaded from apple model gallery

---
+++ ImageClassification-CoreML/LiveImageViewController.swift
@@ -0,0 +1,195 @@
+//
+//  ViewController.swift
+//  MobileNetApp
+//
+//  Created by GwakDoyoung on 28/05/2018.
+//  Copyright © 2018 GwakDoyoung. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class LiveImageViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    @IBOutlet weak var videoPreview: UIView!
+    @IBOutlet weak var labelLabel: UILabel!
+    @IBOutlet weak var confidenceLabel: UILabel!
+    
+    @IBOutlet weak var inferenceLabel: UILabel!
+    @IBOutlet weak var etimeLabel: UILabel!
+    @IBOutlet weak var fpsLabel: UILabel!
+    
+    // MARK - Performance Measurement Property
+    private let 👨‍🔧 = 📏()
+    
+    // MARK - Core ML model
+    // MobileNet(iOS11+), MobileNetV2(iOS11+), MobileNetV2FP16(iOS11.2+), MobileNetV2Int8LUT(iOS12+)
+    // Resnet50(iOS11+), Resnet50FP16(iOS11.2+), Resnet50Int8LUT(iOS12+), Resnet50Headless(N/A)
+    // SqueezeNet(iOS11+), SqueezeNetFP16(iOS11.2+), SqueezeNetInt8LUT(iOS12+)
+    let classificationModel = MobileNetV2()
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    
+    // MARK: - AV Properties
+    var videoCapture: VideoCapture!
+    
+    
+    // MARK: - View Controller Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup ml model
+        setUpModel()
+        
+        // setup camera
+        setUpCamera()
+        
+        // setup delegate for performance measurement
+        👨‍🔧.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.videoCapture.start()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.videoCapture.stop()
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        if let visionModel = try? VNCoreMLModel(for: classificationModel.model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+        } else {
+            fatalError()
+        }
+    }
+    
+    
+    // MARK: - 초기 세팅
+    
+    func setUpCamera() {
+        videoCapture = VideoCapture()
+        videoCapture.delegate = self
+        videoCapture.fps = 50
+        videoCapture.setUp(sessionPreset: .vga640x480) { success in
+            
+            if success {
+                // UI에 비디오 미리보기 뷰 넣기
+                if let previewLayer = self.videoCapture.previewLayer {
+                    self.videoPreview.layer.addSublayer(previewLayer)
+                    self.resizePreviewLayer()
+                }
+                
+                // 초기설정이 끝나면 라이브 비디오를 시작할 수 있음
+                self.videoCapture.start()
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        resizePreviewLayer()
+    }
+    
+    func resizePreviewLayer() {
+        videoCapture.previewLayer?.frame = videoPreview.bounds
+    }
+}
+
+// MARK: - VideoCaptureDelegate
+extension LiveImageViewController: VideoCaptureDelegate {
+    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame pixelBuffer: CVPixelBuffer?/*, timestamp: CMTime*/) {
+        
+        // 카메라에서 캡쳐된 화면은 pixelBuffer에 담김.
+        // Vision 프레임워크에서는 이미지 대신 pixelBuffer를 바로 사용 가능
+        if let pixelBuffer = pixelBuffer {
+            // start of measure
+            self.👨‍🔧.🎬👏()
+            
+            // start predict
+            self.predictUsingVision(pixelBuffer: pixelBuffer)
+        }
+    }
+}
+
+// MARK: - Inference
+extension LiveImageViewController {
+    func predictUsingVision(pixelBuffer: CVPixelBuffer) {
+        guard let request = request else { fatalError() }
+        // vision framework configures the input size of image following our model's input configuration automatically
+        let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+        try? handler.perform([request])
+    }
+    
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        // middle of measure
+        self.👨‍🔧.🏷(with: "endInference")
+        
+        // 메인큐에서 결과 출력
+        if let classificationResults = request.results as? [VNClassificationObservation] {
+            showClassificationResult(results: classificationResults)
+        } else if let mlFeatureValueResults = request.results as? [VNCoreMLFeatureValueObservation] {
+            showCustomResult(results: mlFeatureValueResults)
+        }
+        
+        DispatchQueue.main.sync {
+            // end of measure
+            self.👨‍🔧.🎬🤚()
+        }
+    }
+    
+    func showClassificationResult(results: [VNClassificationObservation]) {
+        guard let result = results.first else {
+            showFailResult()
+            return
+        }
+        
+        showResults(objectLabel: result.identifier, confidence: result.confidence)
+    }
+    
+    func showCustomResult(results: [VNCoreMLFeatureValueObservation]) {
+        guard let result = results.first else {
+            showFailResult()
+            return
+        }
+        
+        showFailResult() // TODO
+    }
+    
+    func showFailResult() {
+        DispatchQueue.main.sync {
+            self.labelLabel.text = "n/a result"
+            self.confidenceLabel.text = "-- %"
+        }
+    }
+    
+    func showResults(objectLabel: String, confidence: VNConfidence) {
+        DispatchQueue.main.sync {
+            self.labelLabel.text = objectLabel
+            self.confidenceLabel.text = "\(round(confidence * 100)) %"
+        }
+    }
+}
+
+// MARK: - 📏(Performance Measurement) Delegate
+extension LiveImageViewController: 📏Delegate {
+    func updateMeasure(inferenceTime: Double, executionTime: Double, fps: Int) {
+        //print(executionTime, fps)
+        self.inferenceLabel.text = "inference: \(Int(inferenceTime*1000.0)) mm"
+        self.etimeLabel.text = "execution: \(Int(executionTime*1000.0)) mm"
+        self.fpsLabel.text = "fps: \(fps)"
+    }
+}
---
+++ ImageClassification-CoreML/StillImageViewController.swift
@@ -0,0 +1,92 @@
+//
+//  StillImageViewController.swift
+//  MobileNetApp
+//
+//  Created by Doyoung Gwak on 20/07/2019.
+//  Copyright © 2019 GwakDoyoung. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class StillImageViewController: UIViewController {
+    
+    // MARK: - UI Properties
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var top1ResultLabel: UILabel!
+    @IBOutlet weak var top1ConfidenceLabel: UILabel!
+    
+    let imagePickerController = UIImagePickerController()
+    
+    // MARK - Core ML model
+    // MobileNet(iOS11+), MobileNetV2(iOS11+), MobileNetV2FP16(iOS11.2+), MobileNetV2Int8LUT(iOS12+)
+    // Resnet50(iOS11+), Resnet50FP16(iOS11.2+), Resnet50Int8LUT(iOS12+), Resnet50Headless(N/A)
+    // SqueezeNet(iOS11+), SqueezeNetFP16(iOS11.2+), SqueezeNetInt8LUT(iOS12+)
+    let classificationModel = MobileNetV2()
+    
+    // MARK: - Vision Properties
+    var request: VNCoreMLRequest?
+    var visionModel: VNCoreMLModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup ml model
+        setUpModel()
+        
+        // image picker delegate setup
+        imagePickerController.delegate = self
+    }
+    
+    @IBAction func tapImport(_ sender: Any) {
+        self.present(imagePickerController, animated: true)
+    }
+    
+    // MARK: - Setup Core ML
+    func setUpModel() {
+        if let visionModel = try? VNCoreMLModel(for: classificationModel.model) {
+            self.visionModel = visionModel
+            request = VNCoreMLRequest(model: visionModel, completionHandler: visionRequestDidComplete)
+            request?.imageCropAndScaleOption = .scaleFill
+        } else {
+            fatalError()
+        }
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate & UINavigationControllerDelegate
+extension StillImageViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage,
+            let url = info[UIImagePickerControllerImageURL] as? URL {
+            mainImageView.image = image
+            self.predict(with: url)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Inference
+extension StillImageViewController {
+    // prediction
+    func predict(with url: URL) {
+        guard let request = request else { fatalError() }
+        
+        // vision framework configures the input size of image following our model's input configuration automatically
+        let handler = VNImageRequestHandler(url: url, options: [:])
+        try? handler.perform([request])
+    }
+    
+    // post-processing
+    func visionRequestDidComplete(request: VNRequest, error: Error?) {
+        print(request)
+        
+        if let result = request.results?.first as? VNClassificationObservation {
+            top1ResultLabel.text = result.identifier
+            top1ConfidenceLabel.text = "\(String(format: "%.2f", result.confidence * 100))%"
+        } else {
+            top1ResultLabel.text = "no result"
+            top1ConfidenceLabel.text = "--%"
+        }
+    }
+}
